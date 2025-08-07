9fe7874a741583532a3201219d6486a9dff32cf4
Enver Yalçın <srb8env@outlook.com>
2025-08-07 19:37:33 +0300

    Update ortak.py

--- ortak.py
+++ ortak.py
@@ -5,7 +5,7 @@ m3u_urls = [
     "https://raw.githubusercontent.com/koprulu5564/kablo-tv-mod/refs/heads/main/playlist.m3u",
     "https://raw.githubusercontent.com/koprulu5564/rectv/refs/heads/main/rectv_full.m3u",
     "https://raw.githubusercontent.com/koprulu5564/trgoals/refs/heads/main/trgoals.m3u",
-    "https://raw.githubusercontent.com/mehmetey03/METV/refs/heads/main/selcuk.m3u",
+    "https://raw.githubusercontent.com/Love4vn/love4vn/refs/heads/main/Mac2M3uPlaylist_SPORT.m3u",
 ]
 
 # Çıktı dosyası adı
