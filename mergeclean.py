804d204c0f690ba80d094353e361dc26ecb15605
Enver Yalçın <srb8env@outlook.com>
2025-09-10 11:19:21 +0300

    Update mergeclean.py

--- mergeclean.py
+++ mergeclean.py
@@ -10,7 +10,7 @@ playlist_urls = [
     "https://raw.githubusercontent.com/karams81/voddensrb/refs/heads/main/trgoals.m3u",
     "https://raw.githubusercontent.com/primatzeka/kurbaga/refs/heads/main/NeonSpor/NeonSpor.m3u",
     "https://raw.githubusercontent.com/Kutlu0319/krnagrsi/refs/heads/main/umitm0d.m3u", 
-    https://raw.githubusercontent.com/mehmetey03/raply/refs/heads/main/metv/S%C3%BCper_Lig_2025-2026/S%C3%BCper_Lig_2025-2026.m3u
+    "https://raw.githubusercontent.com/mehmetey03/raply/refs/heads/main/metv/S%C3%BCper_Lig_2025-2026/S%C3%BCper_Lig_2025-2026.m3u",
 ]
 
 UDPTV_URL = "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/UDPTV.m3u"
